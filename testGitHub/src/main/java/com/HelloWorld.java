1488fa82926e1ae9ff75ecd1587ccaceaff9c249
Evgeniy Radchin <evgeniy.radchin@stud.onu.edu.ua>
2021-05-24 00:49:09 +0300

    second commit

--- testGitHub/src/main/java/com/HelloWorld.java
+++ testGitHub/src/main/java/com/HelloWorld.java
@@ -3,5 +3,8 @@ package com;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int b = 3, c = 5;
+        int a = b + c;
+        System.out.println(a);
     }
 }
