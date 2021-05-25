a34efb1906d711106e66d7c58ab5220f91ad56a1
Evgeniy Radchin <evgeniy.radchin@stud.onu.edu.ua>
2021-05-25 22:21:46 +0300

    Revert "third commit"
    This reverts commit 36c1c82f6add939afd4d87ed9c6e873eb7230e48.

--- testGitHub/src/main/java/com/HelloWorld.java
+++ testGitHub/src/main/java/com/HelloWorld.java
@@ -3,7 +3,7 @@ package com;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int b = 3, c = 5, v = 9;
+        int b = 3, c = 5;
         int a = b + c;
         System.out.println(a);
     }
